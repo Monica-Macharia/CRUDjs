a5f45a906973f42526c3f0f57e8272cf4bb5df25
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-01-04 07:06:11 +0300

    eventlstener to forms

--- crud.js
+++ crud.js
@@ -2,18 +2,38 @@
 //function that does GET request and pushes the data into a display function using a ,map or .forEah.
 //display function that pushes the data to a specific location on the DOM.
 
-function displayFunction(data){
-    let location = document.querySelector("#data")
-    let divi = document.createElement("div")
-    divi.innerHTML = 
-    `<img src = ${data.image}>
-    <p>${data.name}</p>`
-    console.log(data)
+// function displayFunction(data){
+//     let location = document.querySelector("#data")
+//     let divi = document.createElement("div")
+//     divi.innerHTML = 
+//     `<img src = ${data.image}>
+//     <p>${data.name}</p>`
+//     console.log(data)
 
-    let newDiv = location.appendChild(divi)
-    console.log(newDiv)
+//     let newDiv = location.appendChild(divi)
+//     console.log(newDiv)
+// }
+
+// fetch('http://localhost:3000/animals')
+// .then(response => response.json())
+// .then(data => data.map(item => displayFunction(item)))
+
+//task
+//place an eventlistener on form submit button
+//grab the incoming value
+//customize fetch to the incoming value
+//push the fetch results to the DOM
+
+//step1 add a eventlistener, prevent default
+
+function searcher(){
+    document.querySelector("form").addEventListener("submit", function(e){
+        e.preventDefault()
+        let input = document.querySelector("input#search").value
+        console.log(input)
+        form.reset()
+    })
+
+    fetch("")
 }
-
-fetch('http://localhost:3000/animals')
-.then(response => response.json())
-.then(data => data.map(item => displayFunction(item)))
+searcher();
