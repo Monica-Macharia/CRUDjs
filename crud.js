2af71a27919c08f6434f9453825bbfb13b5011a3
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-01-04 07:45:28 +0300

    feature: search

--- crud.js
+++ crud.js
@@ -2,21 +2,22 @@
 //function that does GET request and pushes the data into a display function using a ,map or .forEah.
 //display function that pushes the data to a specific location on the DOM.
 
-// function displayFunction(data){
-//     let location = document.querySelector("#data")
-//     let divi = document.createElement("div")
-//     divi.innerHTML = 
-//     `<img src = ${data.image}>
-//     <p>${data.name}</p>`
-//     console.log(data)
+function displayFunction(data){
+    let location = document.querySelector("#data")
+    let divi = document.createElement("div")
+    divi.innerHTML = 
+    `<img src = ${data.image}>
+    <p>Id: ${data.id}</p>
+    <p>Name: ${data.name}</p>`
+    console.log(data)
 
-//     let newDiv = location.appendChild(divi)
-//     console.log(newDiv)
-// }
+    let newDiv = location.appendChild(divi)
+    console.log(newDiv)
+}
 
-// fetch('http://localhost:3000/animals')
-// .then(response => response.json())
-// .then(data => data.map(item => displayFunction(item)))
+fetch('http://localhost:3000/animals')
+.then(response => response.json())
+.then(data => data.map(item => displayFunction(item)))
 
 //task
 //place an eventlistener on form submit button
@@ -28,12 +29,22 @@
 
 function searcher(){
     document.querySelector("form").addEventListener("submit", function(e){
-        e.preventDefault()
+        e.preventDefault();
         let input = document.querySelector("input#search").value
         console.log(input)
-        form.reset()
+       
+        
+    fetch(`http://localhost:3000/animals/${input}`)
+    .then(res => res.json())
+    .then(data => {
+        let places = document.querySelector("div#data");
+        places.innerHTML =         
+         `<img src = ${data.image}>`
+       
+        
+    })
     })
 
-    fetch("")
+    
 }
 searcher();
